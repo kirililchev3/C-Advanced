83ea72342306741197cc24b2002f98e1db7f6ef4
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-18 22:25:51 +0200

    Add files via upload

---
+++ Stack and Queues/Basic Stack Operations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] commands = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var stack = new Stack<int>();
+            for (int i = 0; i < commands[0] && i < numbers.Length; i++)
+            {
+              
+                stack.Push(numbers[i]);
+                
+            }
+
+            for (int i = 0; i < commands[1] && stack.Count > 0; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                if (stack.Contains(commands[2]))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine($"{stack.Min()}");
+                }
+            }
+            
+            
+            
+        }
+    }
+}
