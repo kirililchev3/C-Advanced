bde1b2f2e2d83458890fd927dc9b6907fbd12d86
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-18 22:33:56 +0200

    Add files via upload

---
+++ Stack and Queues/Print Even Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var queue = new Queue<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    queue.Enqueue(nums[i]);
+                }
+            }
+            Console.WriteLine(string.Join(", ",queue.ToArray()));
+
+            
+            
+        }
+    }
+}
