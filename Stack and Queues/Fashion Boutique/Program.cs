178c7d41c6a2104566f15205e469693b7709af29
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-18 22:27:12 +0200

    Add files via upload

---
+++ Stack and Queues/Fashion Boutique/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothesInBox = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int capacityOfRack = int.Parse(Console.ReadLine());
+            var stack = new Stack<int>(clothesInBox);
+            int countOfRack = 1;
+            int sum = 0;
+
+            while (stack.Count > 0)
+            {
+                int currentClothValue = stack.Pop();
+                if (capacityOfRack - (sum + currentClothValue) > 0)
+                {
+                    sum += currentClothValue;
+                }
+                else if (capacityOfRack - (sum + currentClothValue) == 0)
+                {
+                    if (stack.Count != 0)
+                    {
+                        countOfRack++;
+                        sum = 0;
+                    }
+                }
+                else if (capacityOfRack - (sum + currentClothValue) < 0)
+                {
+                    stack.Push(currentClothValue);
+                    countOfRack++;
+                    sum = 0;
+                }
+            }
+
+            Console.WriteLine(countOfRack);
+
+        }
+    }
+}
+
