ab3ca232dded970a9ab5ed3f92d4e90a14af8cdf
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-18 22:30:47 +0200

    Add files via upload

---
+++ Stack and Queues/Matching Brackets/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char ch = input[i];
+                if (ch == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (ch == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int length = i - startIndex + 1;
+                    string contents = input.Substring(startIndex, length);
+                    Console.WriteLine(contents);
+                }
+                
+            }
+            
+        }
+    }
+}
