5efda3a7e37f37aa7a5715c80402222cd2960f7d
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-18 22:38:51 +0200

    Add files via upload

---
+++ Stack and Queues/Traffic Jam/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersOfCarsToPass = int.Parse(Console.ReadLine());
+            var queue = new Queue<string>();
+            int counter = 0;
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "green")
+                {
+                    int min = Math.Min(numbersOfCarsToPass, queue.Count);
+                    for (int i = 0; i < min; i++)
+                    {
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        counter++;
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+            }
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
