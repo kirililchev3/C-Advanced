5de7f55a4d09f4b635b86ba2bfcb069c67111ba3
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-18 22:24:44 +0200

    Add files via upload

---
+++ Stack and Queues/Basic Queue Operations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] commands = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var queue = new Queue<int>();
+
+            for (int i = 0; i < commands[0] && i < numbers.Length; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+
+            for (int i = 0; i < commands[1] && queue.Count > 0; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                if (queue.Contains(commands[2]))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine($"{queue.Min()}");
+                }
+            }
+        }
+    }
+}
