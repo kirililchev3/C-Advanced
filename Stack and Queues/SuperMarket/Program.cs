4b7a9d7e9e895913644315c11cc265063e94852d
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-18 22:37:56 +0200

    Add files via upload

---
+++ Stack and Queues/SuperMarket/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SuperMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue<string>();
+            
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "End")
+                {
+                    break;
+                }
+                if (command != "Paid")
+                {
+                    queue.Enqueue(command);
+                }
+                 else if (command == "Paid")
+                {
+                    for (int i = queue.Count - 1; i >= 0; i--)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                    
+                }
+
+                
+                
+            }
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
