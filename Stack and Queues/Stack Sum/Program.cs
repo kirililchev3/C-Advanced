d56d1b3998b832a8e64a8d6000ca287d76a393b2
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-18 22:36:58 +0200

    Add files via upload

---
+++ Stack and Queues/Stack Sum/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var stack = new Stack<int>(numbers);
+
+            while (true)
+            {
+
+                string[] tokens = Console.ReadLine().Split();
+                var command = tokens[0].ToLower();
+                if (command == "end")
+                {
+                    break;
+                }
+
+                if (command == "add")
+                {
+
+                    stack.Push(int.Parse(tokens[1]));
+                    stack.Push(int.Parse(tokens[2]));
+
+                }
+                else if (command == "remove")
+                {
+                    int countOfnUmForRemove = int.Parse(tokens[1]);
+                    if (countOfnUmForRemove > stack.Count)
+                    {
+                        continue;
+                    }
+
+                    for (int i = 0; i < countOfnUmForRemove; i++)
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+            var sum = stack.Sum();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
