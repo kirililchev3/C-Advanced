64a9f7086ecfebe9c4e0fe200caa2765646a3a17
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-18 22:28:15 +0200

    Add files via upload

---
+++ Stack and Queues/Fast Food/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fast_Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantity = int.Parse(Console.ReadLine());
+            int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var queue = new Queue<int>(orders);
+            int sum = 0;
+
+            Console.WriteLine(queue.Max());
+            if (queue.Sum() <= quantity)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                for (int i = 0; i <orders.Length; i++)
+                {
+                    sum += orders[i];
+                    
+                    if (sum > quantity)
+                    {
+                        break;
+                    }
+
+                    queue.Dequeue();
+
+                }
+                Console.WriteLine($"Orders left: {string.Join(" ",queue.ToArray())}");
+            }
+            
+        }
+    }
+}
