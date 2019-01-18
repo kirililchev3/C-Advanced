1409f54b2ff99eb7a7e430f77f08d39165f7362a
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-18 22:36:05 +0200

    Add files via upload

---
+++ Stack and Queues/Simple Text Editor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfOperations = int.Parse(Console.ReadLine());
+            var texts = new Stack<string>();
+            var text = "";
+            texts.Push(text);
+            for (int i = 0; i < numOfOperations; i++)
+            {
+                var input = Console.ReadLine().Split();
+                var command = input[0];
+                if (command == "1")
+                {
+                    text += input[1];
+                    texts.Push(text);
+                }
+                else if (command == "2")
+                {
+                    var count = int.Parse(input[1]);
+                    text = text.Substring(0, text.Length - count);
+                    texts.Push(text);
+                }
+                else if (command == "3")
+                {
+                    var index = int.Parse(input[1]);
+                    Console.WriteLine(text[index - 1]);
+                }
+                else if (command == "4")
+                {
+                    if (texts.Count >= 2)
+                    {
+                        texts.Pop();
+                        text = texts.Peek();
+                    }
+                }
+            }
+        }
+    }
+}
