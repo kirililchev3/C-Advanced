4c130600eeabef76c6c376cf67521c3db9d95619
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-18 22:23:40 +0200

    Add files via upload

---
+++ Stack and Queues/Balanced Parentheses/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Balanced_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var parentheses = new Stack<char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(' || input[i] == '[' || input[i] == '{')
+                {
+                    parentheses.Push(input[i]);
+                }
+                else
+                {
+                    if (parentheses.Count == 0)
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                    if (input[i] == ')')
+                    {
+                        var lastParenteses = parentheses.Pop();
+                        if (lastParenteses != '(')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                    }
+                    else if (input[i] == ']')
+                    {
+                        var lastParenteses = parentheses.Pop();
+                        if (lastParenteses != '[')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                    }
+                    if (input[i] == '}')
+                    {
+                        var lastParenteses = parentheses.Pop();
+                        if (lastParenteses != '{')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                    }
+                }
+            }
+            if (parentheses.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
