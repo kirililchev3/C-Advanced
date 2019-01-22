2dca688222d2d2bcae6b7c5b84002293952e04bf
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-22 06:19:05 +0200

    Add files via upload

---
+++ Multidimensional-Arrays/Pascal Triangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long[][] triangle = new long[n][];
+            int currentWidth = 1;
+
+            for (int row = 0; row < n; row++)
+            {
+                triangle[row] = new long[currentWidth];
+                long[] currentRow = triangle[row];
+                currentRow[0] = 1;
+                currentRow[currentRow.Length - 1] = 1;
+                currentWidth++;
+
+                if (currentRow.Length > 2)
+                {
+                    for (int i = 1; i < currentRow.Length - 1; i++)
+                    {
+                        long[] previousRow = triangle[row - 1];
+                        long previousRowSum = previousRow[i] + previousRow[i - 1];
+                        currentRow[i] = previousRowSum;
+                    }
+                }
+            }
+
+            foreach (var row in triangle)
+            {
+                Console.WriteLine(string.Join(" ",row));
+            }
+        }
+    }
+}
