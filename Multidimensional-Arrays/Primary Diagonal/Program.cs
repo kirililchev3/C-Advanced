3fbf388f5e2357e413abc51fa889ee9436951674
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-22 06:19:51 +0200

    Add files via upload

---
+++ Multidimensional-Arrays/Primary Diagonal/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[size, size];
+            int primaryDiagonalSum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colNumbers[col];
+                    if (row == col)
+                    {
+                        primaryDiagonalSum += matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine(primaryDiagonalSum);
+
+           
+          
+            
+        }
+    }
+}
