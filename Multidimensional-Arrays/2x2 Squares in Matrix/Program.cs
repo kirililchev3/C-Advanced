1fef6e06341242c88150664b32dc56b27106affc
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-22 05:55:32 +0200

    Add files via upload

---
+++ Multidimensional-Arrays/2x2 Squares in Matrix/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _2x2_Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsAndCols = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = rowsAndCols[0];
+            int cols = rowsAndCols[1];
+            int counter = 0;
+            char[,] matrix = new char[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] symbol = Console.ReadLine()
+                    .Split(new char[] {' '},StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = symbol[col];
+                }
+            }
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    if (matrix[row,col] == matrix[row, col+1] 
+                        && matrix[row,col] == matrix[row+1,col]
+                        && matrix[row,col] == matrix[row+1,col+1])
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
