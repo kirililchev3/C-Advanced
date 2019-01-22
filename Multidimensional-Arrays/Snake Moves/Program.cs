310c9d7cdff5ceee30d0e60590066b27484c1541
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-22 06:20:42 +0200

    Add files via upload

---
+++ Multidimensional-Arrays/Snake Moves/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Snake_Moves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            var matrix = new char[rows, cols];
+            string snake = Console.ReadLine();
+            int snakeIndex = -1;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    snakeIndex++;
+
+                    if (snakeIndex == snake.Length)
+                    {
+                        snakeIndex = 0;
+                    }
+                    matrix[row, col] = snake[snakeIndex];
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(matrix[row,col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
