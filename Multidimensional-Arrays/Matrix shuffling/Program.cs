c58d051cb2e40c74756f6c13a12d42081a36e2bf
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-22 06:12:35 +0200

    Add files via upload

---
+++ Multidimensional-Arrays/Matrix shuffling/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+
+namespace Matrix_shuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimentions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = dimentions[0];
+            int cols = dimentions[1];
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] line = Console.ReadLine()
+                    .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+
+           
+            
+            while (true)
+            {
+                string[] command = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    
+                if (command[0] == "END")
+                {
+                    break;
+                }
+                else if (command[0] == "swap" && command.Length == 5)
+                {
+                    int row1 = int.Parse(command[1]);
+                    int col1 = int.Parse(command[2]);
+                    int row2 = int.Parse(command[3]);
+                    int col2 = int.Parse(command[4]);
+
+                    if ((row1 < 0 || row1 >= rows) 
+                        || (row2 < 0 || row2 >= rows)
+                        || (col1 < 0 || col1 >= cols)
+                        ||(col2 < 0 || col2 >= cols))
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }
+                    else
+                    {
+                        string temp = matrix[row1, col1];
+                        matrix[row1, col1] = matrix[row2, col2];
+                        matrix[row2, col2] = temp;
+
+                        for (int row = 0; row < rows; row++)
+                        {
+                            for (int col = 0; col < cols; col++)
+                            {
+                                Console.Write(matrix[row,col] + " ");
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+               
+
+                
+            }
+        }
+
+       
+    }
+}
