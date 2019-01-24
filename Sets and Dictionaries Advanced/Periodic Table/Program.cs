fbd4306ded84d4898a366e9c1847c30237f9c3a3
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-24 10:41:42 +0200

    Add files via upload

---
+++ Sets and Dictionaries Advanced/Periodic Table/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var periodicTableElements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine().Split();
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    periodicTableElements.Add(elements[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ",periodicTableElements));
+        }
+    }
+}
