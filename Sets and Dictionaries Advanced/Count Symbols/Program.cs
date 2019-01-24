adb997e2a820edada9ec91ff2c105bdf3a20c2d6
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-24 10:34:38 +0200

    Add files via upload

---
+++ Sets and Dictionaries Advanced/Count Symbols/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var symbolCount = new SortedDictionary<char, int>();
+            var text = Console.ReadLine();
+
+            foreach (var symbol in text)
+            {
+                if (!symbolCount.ContainsKey(symbol))
+                {
+                    symbolCount.Add(symbol, 0);
+                }
+                symbolCount[symbol]++;
+            }
+
+            foreach (var currSymbol in symbolCount)
+            {
+                Console.WriteLine($"{currSymbol.Key}: {currSymbol.Value} time/s");
+            }
+        }
+    }
+}
