2a8be950b13a620e600d05b463169ab6928b9215
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-24 10:40:02 +0200

    Add files via upload

---
+++ Sets and Dictionaries Advanced/Even Times/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var evenTimes = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                
+                if (!evenTimes.ContainsKey(num))
+                {
+                    evenTimes.Add(num,1);
+                }
+                evenTimes[num]++;
+            }
+
+            foreach (var kvp in evenTimes)
+            {
+
+                if (kvp.Value % 2 != 0)
+                {
+                    Console.WriteLine(kvp.Key);
+                }
+
+            }
+            
+            
+        }
+    }
+}
