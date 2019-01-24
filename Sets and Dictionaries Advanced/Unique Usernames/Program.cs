e7e95be9defe3ba723fcbb4012e191af35820a35
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-24 10:55:38 +0200

    Add files via upload

---
+++ Sets and Dictionaries Advanced/Unique Usernames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var uniqueNames = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                uniqueNames.Add(name);
+            }
+
+            foreach (var names in uniqueNames)
+            {
+                Console.WriteLine(names);
+            }
+        }
+    }
+}
