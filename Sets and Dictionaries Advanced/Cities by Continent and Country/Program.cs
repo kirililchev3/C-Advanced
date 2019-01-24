93df35c3370955278b0dafa1cb392838f53bca79
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-24 10:32:00 +0200

    Add files via upload

---
+++ Sets and Dictionaries Advanced/Cities by Continent and Country/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cities_by_Continent_and_Country
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var continents = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split();
+                var continent = tokens[0];
+                var country = tokens[1];
+                var city = tokens[2];
+
+                if (!continents.ContainsKey(continent))
+                {
+                    continents[continent] = new Dictionary<string, List<string>>();
+                }
+                if (!continents[continent].ContainsKey(country))
+                {
+                    continents[continent][country] = new List<string>();
+                }
+                continents[continent][country].Add(city);
+            }
+
+            foreach (var kvp in continents)
+            {
+                Console.WriteLine($"{kvp.Key}:");
+                foreach (var cities in kvp.Value)
+                {
+                    Console.WriteLine($"{cities.Key} -> {string.Join(", ",cities.Value)}");
+                }
+            }
+        }
+    }
+}
