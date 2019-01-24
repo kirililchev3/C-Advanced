4dad7bcdc58c32b7d27edb92292df0f10e8f9148
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-24 10:27:59 +0200

    Add files via upload

---
+++ Sets and Dictionaries Advanced/Parking Lot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _6._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var parking = new HashSet<string>();
+
+            while (input != "END")
+            {
+                var inputParams = Regex.Split(input, ", ");
+                if (inputParams[0] == "IN")
+                {
+                    parking.Add(inputParams[1]);
+                }
+                else
+                {
+                    parking.Remove(inputParams[1]);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (parking.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+
+            foreach (var item in parking)
+            {
+                
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
