705cd6ae42c7d8232164d2f3f51f0cad7c51b0d1
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-24 10:49:03 +0200

    Add files via upload

---
+++ Sets and Dictionaries Advanced/Sets of Elements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = nums[0];
+            int m = nums[1];
+            var firstNumbers = new HashSet<int>();
+            var secondNumbers = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                firstNumbers.Add(num);
+            }
+            for (int i = 0; i < m; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                secondNumbers.Add(num);
+            }
+            Console.WriteLine(string.Join(" ",firstNumbers.Intersect(secondNumbers)));
+            
+            
+
+           
+
+        }
+    }
+}
