101802badeabc613276e3016621baec98c87b73c
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-24 10:44:08 +0200

    Add files via upload

---
+++ Sets and Dictionaries Advanced/Product Shop/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Product_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var shopProducts = new SortedDictionary<string, Dictionary<string, double>>();
+            string line = Console.ReadLine();
+            while (line != "Revision")
+            {
+                string[] commands = line.Split(", ",StringSplitOptions.RemoveEmptyEntries);
+                string shop = commands[0];
+                string product = commands[1];
+                double price = double.Parse(commands[2]);
+                if (!shopProducts.ContainsKey(shop))
+                {
+                    shopProducts[shop] = new Dictionary<string, double>();
+                }
+
+                if (!shopProducts[shop].ContainsKey(product))
+                {
+                    shopProducts[shop].Add(product, price);
+                }
+
+                line = Console.ReadLine();
+            }
+
+            foreach (var kvp in shopProducts)
+            {
+                
+                Console.WriteLine($"{kvp.Key}-> ");
+
+                foreach (var item in kvp.Value)
+                {
+                    Console.WriteLine($"Product: {item.Key}, Price: {item.Value}");
+                }
+                
+            }
+        }
+
+        
+    }
+}
