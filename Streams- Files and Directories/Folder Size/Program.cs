310e1fda6a1e182da413f9a9ea2891021378b9b6
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-30 08:38:40 +0200

    Add files via upload

---
+++ Streams- Files and Directories/Folder Size/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Folder_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles(@"D:\Programming\Project's\Streams, Files and Directories\Streams, Files and Directories\Folder Size\Resources\06. Folder Size\TestFolder");
+            double sum = 0;
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                sum += fileInfo.Length;
+            }
+            sum = (sum / 1024) / 1024;
+            File.WriteAllText(@"D:\Programming\Project's\Streams, Files and Directories\Streams, Files and Directories\Folder Size\Resources\06. Folder Size/TestFolder/Size of Files here in MB.txt", sum.ToString());
+
+        }
+    }
+}
