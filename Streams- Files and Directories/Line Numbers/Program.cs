7d8d06c3d3b974f8f3a9849ce232294a7b75c0d7
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-30 08:40:24 +0200

    Add files via upload

---
+++ Streams- Files and Directories/Line Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader(@"Resources\02. Line Numbers\Input.txt"))
+            {
+                string line = reader.ReadLine();
+                int counter = 1;
+                using (var writer = new StreamWriter(@"Resources\02. Line Numbers\Output.txt"))
+                {
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+                        line = reader.ReadLine();
+                        counter++;
+                    }
+                    
+
+                }
+            }
+        }
+    }
+}
