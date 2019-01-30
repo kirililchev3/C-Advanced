86d1d2d6823bee8f96ec2597ff96828d383a934b
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-30 08:35:28 +0200

    Add files via upload

---
+++ Streams- Files and Directories/Even Lines/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string specialSymbols = "-.,!?";
+            int counter = 0;
+
+            using (var reader = new StreamReader(@"../../../text.txt"))
+            {
+                var line = reader.ReadLine();
+
+                using (var writer = new StreamWriter(@"../../../output.txt"))
+                {
+                    while (line != null)
+                    {
+                       
+                        if (counter % 2 == 0)
+                        {
+                            if (counter != 0)
+                            {
+                                writer.WriteLine();
+                            }
+                            string changedLine = string.Empty;
+                            foreach (var charr in line)
+                            {
+                                if (specialSymbols.Contains(charr))
+                                {
+                                    changedLine += '@';
+                                }
+                                else
+                                {
+                                    changedLine += charr;
+                                }
+                            }
+
+                            string[] splitedLine = changedLine.Split();
+
+                            Array.Reverse(splitedLine);
+                            writer.WriteLine(string.Join(" ", splitedLine));
+                        }
+                        counter++;
+                        line = reader.ReadLine();
+                        
+                    }
+                }
+            }
+        }
+    }
+}
