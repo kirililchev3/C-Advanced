94b9b205cf7e957440fc47d5edb091e956f2823c
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-30 08:49:10 +0200

    Add files via upload

---
+++ Streams- Files and Directories/Word Count/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Word_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] keys = File.ReadAllText(@"D:\Programming\Project's\Streams, Files and Directories\Streams, Files and Directories\Word Count\Resources\03. Word Count\words.txt")
+                .ToLower().Split();
+
+            string[] text = File.ReadAllText(@"D:\Programming\Project's\Streams, Files and Directories\Streams, Files and Directories\Word Count\Resources\03. Word Count\text.txt")
+                .ToLower()
+                .Split(new char[] { '\n', '\r', ' ', '.', ',', '!', '?', '-' }
+                , StringSplitOptions.RemoveEmptyEntries);
+
+            File.WriteAllLines(@"D:\Programming\Project's\Streams, Files and Directories\Streams, Files and Directories\Word Count\Resources\03. Word Count\KeysCount.txt", KeysCount(keys, text)
+                .Select(x => $"{x.Key} - {x.Value}"));
+        }
+
+        private static Dictionary<string,int> KeysCount(string[] keys, string[] text)
+        {
+            Dictionary<string, int> keyCount = new Dictionary<string, int>();
+            foreach (var key in keys)
+            {
+                keyCount[key] = 0;
+            }
+
+            foreach (var word in text)
+            {
+                if (keyCount.ContainsKey(word))
+                {
+                    keyCount[word]++;
+                }
+            }
+
+            return keyCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, y => y.Value);
+
+        }
+    }
+}
