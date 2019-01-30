3fa47a7c0b46d9fa3169de9e5db856dac0cea8f2
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-01-30 08:43:54 +0200

    Add files via upload

---
+++ Streams- Files and Directories/Merge Files/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Merge_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] allData = File.ReadAllLines
+                (@"D:\Programming\Project's\Streams, Files and Directories\Streams, Files and Directories\Merge Files\04. Merge Files\FileOne.txt");
+            allData = allData.Concat(File.ReadAllLines(@"D:\Programming\Project's\Streams, Files and Directories\Streams, Files and Directories\Merge Files\04. Merge Files\FileTwo.txt"))
+                .ToArray();
+            File.WriteAllLines(@"D:\Programming\Project's\Streams, Files and Directories\Streams, Files and Directories\Merge Files\04. Merge Files\ThirdFile.txt"
+                , allData.OrderBy(x => x));
+        }
+    }
+}
