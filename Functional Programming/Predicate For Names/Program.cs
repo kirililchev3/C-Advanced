ef53e975ed18fd91b11c83593bf19bb5a622a3de
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-02-04 15:06:22 +0200

    Add files via upload

---
+++ Functional Programming/Predicate For Names/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nameLength = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split();
+
+            Predicate<string> filter = x => x.Length <= nameLength;
+            Func<string, bool> filterFunc = x => filter(x);
+
+            names = names.Where(filterFunc).ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine,names));
+        }
+    }
+}
