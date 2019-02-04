4d8192acc4f2e70cee9c35dd38e81bcfb0312834
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-02-04 15:09:00 +0200

    Add files via upload

---
+++ Functional Programming/Reverse And Exclude/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split().Select(int.Parse);
+            int delimiter = int.Parse(Console.ReadLine());
+
+            Predicate<int> filter = x => x % delimiter != 0;
+            Func<int, bool> filterFunc = x => filter(x);
+
+            nums = nums.Where(filterFunc);
+
+            Console.WriteLine(string.Join(" ",nums.Reverse()));
+
+        }
+    }
+}
