dff1cb9f05533eb593e5533cbd069b36dbbba5c0
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-02-04 15:03:15 +0200

    Add files via upload

---
+++ Functional Programming/List of Predicates/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace List_of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var dividers = Console.ReadLine().Split().Distinct().Select(int.Parse).ToList();
+
+            List<Predicate<int>> predicates = new List<Predicate<int>>();
+            dividers.ForEach(d => predicates.Add(x => x % d == 0));
+            List <int> result = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                bool isDivisible = true;
+
+                foreach (var predicate in predicates)
+                {
+                    if (!predicate(i))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    result.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ",result));
+        }
+    }
+}
