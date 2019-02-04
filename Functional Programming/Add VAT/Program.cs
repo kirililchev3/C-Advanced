3af0dcd5d2691060161351df7044f2f26c01cd7a
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-02-04 14:48:38 +0200

    Add files via upload

---
+++ Functional Programming/Add VAT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine,numbers
+                .Select(p => $"{p*1.20:F2}")));
+               
+
+                
+        }
+
+    }
+}
