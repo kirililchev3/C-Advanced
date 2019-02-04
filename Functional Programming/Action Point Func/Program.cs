b200d7d63ab831800288bf89addeffc45aa4596a
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-02-04 14:46:04 +0200

    Add files via upload

---
+++ Functional Programming/Action Point Func/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Action_Point_Func
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = x => Console.WriteLine(x);
+
+            Console.ReadLine().Split().ToList().ForEach(print);
+        }
+    }
+}
