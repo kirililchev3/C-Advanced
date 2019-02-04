7a52e70093f61798113f3562d20615779da42aa4
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-02-04 14:51:45 +0200

    Add files via upload

---
+++ Functional Programming/Count Uppercase Words/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine()
+                .Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine(string.Join(Environment.NewLine,text.Where(w => char.IsUpper(w[0]))));
+
+        }
+    }
+}
