7a0f10973b8c03b85ff310244806a48ae0e8cf21
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-02-04 14:44:45 +0200

    Add files via upload

---
+++ Functional Programming/Action Point/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Action_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                .Split();
+            Console.WriteLine(string.Join(Environment.NewLine,names));
+                
+
+                
+
+
+        }
+
+    }
+}
