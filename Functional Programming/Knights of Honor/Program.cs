a52c28609e1d7b1dddf8e009595c844bb7c9da29
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-02-04 14:59:17 +0200

    Add files via upload

---
+++ Functional Programming/Knights of Honor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = x => Console.WriteLine($"Sir {x}");
+
+            Console.ReadLine().Split().ToList().ForEach(print);
+
+        }
+    }
+}
