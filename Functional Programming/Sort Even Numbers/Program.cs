b17fd7beedeb72b85ce575a66111b1107b7fede5
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-02-04 15:09:54 +0200

    Add files via upload

---
+++ Functional Programming/Sort Even Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ",numers));
+        }
+    }
+}
