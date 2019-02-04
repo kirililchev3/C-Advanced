4213d2536a37a05dcd30d7516755ee135205c455
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-02-04 14:54:37 +0200

    Add files via upload

---
+++ Functional Programming/Custom Min Function/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Func<int[], int> min = n => n.Min();
+
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(min(nums));
+        }
+    }
+}
