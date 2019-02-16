c5f7ddbbe9aa82325e3d3600062ae4720d128d4d
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-02-16 18:50:27 +0200

    Add files via upload

---
+++ Classes/Speed Racing/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public string model;
+        public decimal fuelAmount;
+        public decimal fuelCost;
+        public decimal distanceTravelled;
+
+        public Car(string model,decimal fuelAmount,decimal fuelCost)
+        {
+            this.model = model;
+            this.fuelAmount = fuelAmount;
+            this.fuelCost = fuelCost;
+            this.distanceTravelled = 0;
+        }
+
+        public void Drive(decimal distance)
+        {
+            if (this.fuelAmount < distance * this.fuelCost)
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+            else
+            {
+                this.fuelAmount -= distance * this.fuelCost;
+                this.distanceTravelled += distance;
+            }
+        }
+    }
+}
---
+++ Classes/Speed Racing/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Car[] cars = new Car[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                string[] tokens = input.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+                string model = tokens[0];
+                decimal fuelAmount = decimal.Parse(tokens[1]);
+                decimal fuelCost = decimal.Parse(tokens[2]);
+                cars[i] = new Car(model, fuelAmount, fuelCost);
+            }
+
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] tokens = command.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+                string model = tokens[1];
+                decimal distance = decimal.Parse(tokens[2]);
+
+                cars.Where(c => c.model == model).ToList().ForEach(c => c.Drive(distance));
+
+                command = Console.ReadLine();
+            }
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine($"{car.model} {car.fuelAmount:f2} {car.distanceTravelled}");
+            }
+        }
+    }
+}
