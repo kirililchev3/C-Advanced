6021ae133d40285f3aaa116c7fde535127a680dd
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-02-16 18:19:55 +0200

    Add files via upload

---
+++ Classes/Creating Constructors/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            var personFirst = new Person();
+            personFirst.Name = "Pesho";
+            personFirst.Age = 20;
+
+            var personSecond = new Person
+            {
+                Name = "Gosho",
+                Age = 18,
+            };
+
+            var personThird = new Person
+            {
+                Name = "Stamat",
+                Age = 43,
+            };
+
+            Console.WriteLine(personFirst.GetIformation());
+            Console.WriteLine(personSecond.GetIformation());
+            Console.WriteLine(personThird.GetIformation());
+
+
+
+
+
+
+
+        }
+    }
+}
