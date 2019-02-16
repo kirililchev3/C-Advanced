0aa12a1728141c1fee512259b688e2c073c9ea66
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-02-16 18:41:57 +0200

    Add files via upload

---
+++ Classes/Oldest Family Member/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+     
+        public Person(string name,int age)
+        {
+            Name = name;
+            Age = age;
+        }
+       
+
+
+
+    }
+}
---
+++ Classes/Oldest Family Member/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Family family = new Family();
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+
+                family.AddMember(new Person(name, age));
+            }
+
+            Person oldestMember = family.GetOldestMember();
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+
+
+
+
+
+        }
+    }
+}
