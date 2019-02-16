04c12b8200c7ac7e0eebf3f6eef83fcf2dd1975d
Kiril Ilchev <39383858+kirililchev3@users.noreply.github.com>
2019-02-16 18:43:17 +0200

    Add files via upload

---
+++ Classes/Opinion Poll/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            
+            int count = int.Parse(Console.ReadLine());
+            Person[] person = new Person[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                string personString = Console.ReadLine();
+                string[] tokens = personString.Split();
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+                person[i] = new Person(name, age);
+            }
+
+            List<Person> result = person.OrderBy(p => p.Name).Where(p => p.Age > 30).ToList();
+
+
+            foreach (var persons in result)
+            {
+                Console.WriteLine($"{persons.Name} - {persons.Age}");
+            }
+          
+
+           
+
+
+
+
+
+        }
+    }
+}
